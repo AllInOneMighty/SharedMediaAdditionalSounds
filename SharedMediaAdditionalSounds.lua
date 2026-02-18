3ff4a5751f4f2b4fd3cbef69223cdc8efc518c80
All-in-one Mighty <mighty@allinonemighty.com>
2026-02-18 12:24:29 -0800

    Naming

--- SharedMediaAdditionalSounds.lua
+++ SharedMediaAdditionalSounds.lua
@@ -10,7 +10,7 @@ create your own addon or use an existing one if there is one.
 Don't add a sound that you did not test. All sounds must be tested in game before upload.
 
 Thanks for your cooperation! ;-)
-pb_ee1
+All-in-one Mighty
 
 If you edit this file, be careful not to overwrite it when you update this addon.
 
